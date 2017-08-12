c837e69cf97d77d593ab26d546779bf577d1afe0
Dmitry Khomikh <dmitrykhomikh@homeportal>
2017-08-11 19:41:08 -0700

    8-11-2017[dmitry] new class

---
+++ src/wikia/Dancer.java
@@ -0,0 +1,12 @@
+package wikia;
+
+
+public class Dancer extends Singer {
+	
+	public void Dance(){
+		System.out.println("I can dance");
+	}
+
+	//("I can dance");
+
+}
---
+++ src/wikia/Singer.java
@@ -0,0 +1,17 @@
+
+package wikia;
+
+import java.util.Hashtable;
+
+public class Singer extends Human {
+	
+	public Hashtable<String, String> job = new Hashtable<String, String>();
+	
+	public void Sing (){
+		System.out.println("I am Singing");
+		
+	}
+
+}
+
+
